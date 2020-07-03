02c4220889c387572b3e7885b328affd3f130964
Timo Schinkel <timo.schinkel@coolblue.nl>
2020-07-03 17:11:58 +0200

    Add json generation and move html to `docs`
    Add step 1; generation of the json files. These json files will serve as source for the html files.

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 I'm not ashamed to admit I still like to play [Pokémon](https://www.pokemon.com/us/). And as I'm a bit of a [completionist](https://www.dictionary.com/browse/completionist) I like to achieve a [living Pokédex](https://bulbapedia.bulbagarden.net/wiki/Living_Pok%C3%A9dex) in every game I play.
 
 This repository is a helping hand in organizing my living Pokédex. For now the only supported Pokédex is for the [Galar region](https://bulbapedia.bulbagarden.net/wiki/Galar). I hope to expand this to all regions later.
+
+The data is based on the [veekun/pokedex](https://github.com/veekun/pokedex), except for the Galar data as this is not yet available in Veekun.
+
+## Building the templates
+As an experiment the generation process is built using [Go](https://golang.org).
+
+The process of building the templates consists of two steps; generating JSON and generating HTML. The JSON files are committed to Git in the folder `/data`. This step should only be necessary when new content is made available.
+
+### Generating JSON sources
+First build the veekun/pokedex database following the [instructions](https://github.com/veekun/pokedex/wiki/Getting-Data). The database should be built to Sqlite. Make sure the resulting Sqlite data is copied to the working directory of this repository. The Sqlite database is dumped to `pokedex/data/pokedex.sqlite` by default.
+
+Next steps are preparation, compilation and generation:
+- `docker-compose run go-cli go install github.com/mattn/go-sqlite3` - This command will install the third party Sqlite driver. This command only needs to run once after the first creation of the docker container.
+- `docker-compose run go-cli go build generate_json.go` - This command will compile the generation script.
+- `docker-compose run go-cli ./generate_json` - This command will run the compiled binary and will do the actual work.
+
+After running these three commands a batch of JSON files should be created in `/data`.
+
+### Generating HTML
---
+++ generate_json.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Pokedex struct {
+	Generation string 		`json:"generation"`
+	Identifier string		`json:"identifier"`
+	Name string				`json:"name"`
+	Games []string			`json:"games"`
+	Region string			`json:"region"`
+	MaxNationalDex int		`json:"-"`
+	Pokemon []Pokemon		`json:"pokemon"`
+}
+
+type Pokemon struct {
+	Number int				`json:"number"`
+	Identifier string		`json:"identifier"`
+	Name string				`json:"name"`
+}
+
+var pokedexes = []Pokedex{ 
+	Pokedex{"I", "red-blue-yellow", "Kanto dex", []string{ "Red", "Blue", "Yellow" }, "Kanto", 151, []Pokemon{} },
+	Pokedex{"II", "gold-silver-crystal", "National Johto dex", []string{ "Gold", "Silver", "Crystal" }, "Johto", 251, []Pokemon{} },
+	Pokedex{"III", "ruby-sapphire-emerald", "National Hoenn dex", []string{ "Ruby", "Sapphire", "Emerald" }, "Hoenn", 386, []Pokemon{} },
+	Pokedex{"III", "firered-leafgreen", "National updated Kanto dex", []string{ "FireRed", "Leafgreen" }, "Kanto", 386, []Pokemon{} },
+	Pokedex{"IV", "diamond-pearl-platinum", "National Sinnoh dex", []string{ "Diamond", "Pearl", "Platinum" }, "Sinnoh", 490, []Pokemon{} },
+	Pokedex{"IV", "heartgold-soulsilver", "National updated Johto dex", []string{ "HeartGold", "SoulSilver" }, "Johto", 490, []Pokemon{} },
+	Pokedex{"V", "black-white-black-2-white-2", "National Unova dex", []string{ "Black", "White", "Black 2", "White 2" }, "Unova", 649, []Pokemon{} },
+	Pokedex{"VI", "x-y", "National Kalos dex", []string{ "X", "Y" }, "Kalos", 718, []Pokemon{} },
+	Pokedex{"VI", "omega-ruby-alpha-sapphire", "Nation updated Hoenn dex", []string{ "Omega Ruby", "Alpha Sapphire" }, "Hoenn", 718, []Pokemon{} },
+	Pokedex{"VII", "sun-moon", "Nation Alola dex", []string{ "Sun", "Moon" }, "Alola", 802, []Pokemon{} },
+	Pokedex{"VII", "ultra-sun-ultra-moon", "National updated Alola dex", []string{ "Ultra Sun", "Ultra Moon" }, "Alola", 807, []Pokemon{} },
+	// generation VIII is not yet available in Veekun
+}
+
+func main() {
+	fmt.Printf("Living Pokédex Templates generator\n")
+
+	db, err := sql.Open("sqlite3", "./pokedex.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close() // close connection after `main` returns
+
+	fmt.Printf("Connected to database (%T)\n", db)
+
+	for _, pokedex := range pokedexes {
+		rows, err := db.Query(`
+			select
+				pokemon_species.id as id,
+				pokemon_species.identifier as identifier,
+				pokemon_species_names.name
+			from 
+				pokemon_species
+				join pokemon_species_names on pokemon_species.id = pokemon_species_names.pokemon_species_id and pokemon_species_names.local_language_id = 9
+			where 
+				pokemon_species.id <= ?
+			order by
+				pokemon_species.id asc`, pokedex.MaxNationalDex)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pokedex.Pokemon = make([]Pokemon, pokedex.MaxNationalDex)
+
+		defer rows.Close() // close result set after operations
+		for rows.Next() {
+			var id int
+			var identifier, name string
+
+			err = rows.Scan(&id, &identifier, &name)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			pokedex.Pokemon[id - 1] = Pokemon{id, identifier, name}
+		}
+
+		json, err := json.MarshalIndent(pokedex, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = ioutil.WriteFile("data/" + pokedex.Identifier + ".json", json, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Written \"%s\" to data/%s.json\n", pokedex.Name, pokedex.Identifier)
+	}
+}
